f77034081e50be1e8f8cf839f9dbe459ddaf84b9
gracyraghav04 <gracyraghav.04@gmail.com>
2026-03-31 13:29:51 +0530

    Change variable declarations from let to const

--- to-do-list.js
+++ to-do-list.js
@@ -1,20 +1,20 @@
 console.log("JS connected");
 
-let btn = document.getElementById("btn");
+const btn = document.getElementById("btn");
 
 btn.addEventListener("click", () => {
-    let value = document.getElementById("task").value;
+    const value = document.getElementById("task").value;
 
     if (value === "") return;
 
-    let li = document.createElement("li");
+    const li = document.createElement("li");
    li.classList.add("item");
-   let text=document.createElement("span");
+   const text=document.createElement("span");
    text.innerHTML=value;
    //let delbtn=document.createElement("button");
 
 
-    let delbtn=document.createElement("button");
+    const delbtn=document.createElement("button");
     delbtn.innerHTML="Remove";
     delbtn.addEventListener("click", ()=> {
       li.remove();
